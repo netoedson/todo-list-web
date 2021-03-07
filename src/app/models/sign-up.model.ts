0a44593f660ec0b375e431f396af8acb0af9115f
Edson Neto <edsonnetowr@gmail.com>
2021-03-07 10:24:52 -0300

    New: included new models

---
+++ src/app/models/sign-up.model.ts
@@ -0,0 +1,8 @@
+export class SignUp {
+  constructor(
+    public name: string = "",
+    public email: string = "",
+    public password: string = "",
+    public confirmPassword: string = ""
+  ) {}
+}
