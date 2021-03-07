427d382ef8d36eb376d05684e84f23614ad88c54
Edson Neto <edsonnetowr@gmail.com>
2021-03-07 16:23:53 -0300

    Update: implemented the home component and module

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ProjectApi } from '../api/project/project.api';
+import { Project } from '../models/project.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public $projects: Observable<Project[]>;
 
-  constructor() { }
+  constructor(private readonly projectService: ProjectApi) {}
 
   ngOnInit(): void {
+    this.$projects = this.projectService
+      .listProjects()
+      .pipe(map((result) => result.projects));
   }
-
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProjectCardComponent } from './project-card/project-card.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskItemComponent } from './task-list/task-item/task-item.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    HomeComponent,
+    NavbarComponent,
+    ProjectCardComponent,
+    TaskListComponent,
+    TaskItemComponent,
+  ],
+  imports: [CommonModule, FormsModule],
 })
-export class HomeModule { }
+export class HomeModule {}
